fed74d6edacf003a978a006d67cf207ead3bbe0d
Mingo <www.kxmd@qq.com>
2017-07-11 11:02:42 +0800

    First commit
    Sort methods

---
+++ insert.py
@@ -0,0 +1,14 @@
+def insert_sort(lists):
+    # 插入排序
+    count = len(lists)
+    for i in range(1, count):
+        key = lists[i]
+        j = i - 1
+        while j >= 0:
+            if lists[j] > key:
+                lists[j + 1] = lists[j]
+                lists[j] = key
+            j -= 1
+    return lists
+a = [1,2,3,7,5,4]
+print(insert_sort(a))
